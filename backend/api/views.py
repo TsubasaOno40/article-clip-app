b82b53018888fc8786819ca6e4d1b8242b6433a4
Tsubasa Ono <o.tsubasa.0707@gmail.com>
2021-11-12 22:34:39 +0900

    forth commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from rest_framework.exceptions import ValidationError
+from rest_framework import status
+from rest_framework.response import Response
+from django.db.models import Q
+from . import serializers
+from .models import Profile, Post, Comment, FriendRequest
 
-# Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = serializers.UserSerializer
+    permission_classes = (AllowAny,)
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(userProfile=self.request.user)
+
+class MyProfileListView(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(userProfile=self.request.user)
+
+class FriendRequestViewSet(viewsets.ModelViewSet):
+    queryset = FriendRequest.objects.all()
+    serializer_class = serializers.FriendRequestSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(Q(askTo=self.request.user) | Q(askFrom=self.request.user))
+
+    def perform_create(self, serializer):
+        try:
+            serializer.save(askFrom=self.request.user)
+        except:
+            raise ValidationError("User can have only unique request")
+
+    def destroy(self, request, *args, **kwargs):
+        response = {'message': 'Delete is not allowed !'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+    def partial_update(self, request, *args, **kwargs):
+        response = {'message': 'Patch is not allowed !'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(userPost=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(userComment=self.request.user)
